f8bb08e607f826ae68ab37582991dff6f01a98e8
Richard T Swierk <18kills.18kills@gmail.com>
2018-01-06 14:36:53 -0500

    Update isUp.py

--- isUp.py
+++ isUp.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 import socket
+import os
 
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 localIP=s.getsockname()[0]
 s.close()
 print(localIP)
+localIP=localIP[:localIP.find(".",localIP.find(".")+localIP.find(".",localIP.find("."))+localIP.find(".",localIP.find(".",localIP.find("."))),)]
+for x in range(255):
+    ping=os.system("ping -c 5 ")
